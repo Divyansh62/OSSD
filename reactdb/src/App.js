9754a041b3e351b9d0d82e0a40a74ed2d0799f71
Divyansh Bhargava <divyanshbhargava79@gmail.com>
2022-05-24 22:00:06 +0530

    Open Source project

---
+++ reactdb/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+//import Restaurant from './component/basics/Restaurant'
+// import UseState from './component/basics/hooks/useState'
+// import UseEffect from './component/basics/hooks/useEffect'
+// import UseReducer from './component/basics/hooks/useReducer'
+import Todo from './component/todoreact/todo'
+
+const App = () => {
+  return (
+    <div>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
---
+++ reactdb/src/component/basics/MenuCard.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const MenuCard = ({menudata}) => {
+ 
+  return (
+    <>
+    <section className='main-card--cointainer'>
+    {menudata.map((curElem)=>{
+
+       const{id,name,category,image,description,price}=curElem;
+
+        return(<div className='card-container' key={curElem.id}>
+        <div className='card'>
+            <div className='card-body'>
+                <span className='card-number card-circle subtle'>{id}</span>
+                <span className='card-author subtle'>{category}</span>
+                <h2 className='card-title'>{name}</h2>
+                <span className='card-description subtle'>
+                {description}
+                </span>
+                <div className='card-read'>{price}</div>
+
+            </div>
+            <img src={curElem.image} alt="images" className='card-media'/>
+            <span className='card-tag subtle'>Order Now</span>
+        </div>
+    </div>)
+    })}
+    </section>
+    </>
+  )
+}
+
+export default MenuCard
